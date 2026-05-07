ce0757e1315f347b1adf3c28519255058ac50ebe
Vapesito <jbinact@gmail.com>
2026-05-06 22:16:34 -0700

    fix: separate 1.21.111 and 1.26.20 palettes to fix cross-version desync

--- src/network/mcpe/convert/BlockTranslator.php
+++ src/network/mcpe/convert/BlockTranslator.php
@@ -55,20 +55,20 @@ final class BlockTranslator{
 			self::BLOCK_STATE_META_MAP_PATH => '',
 		],
 		ProtocolInfo::PROTOCOL_1_21_130 => [
-			self::CANONICAL_BLOCK_STATES_PATH => '',
-			self::BLOCK_STATE_META_MAP_PATH => '',
+			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.111',
+			self::BLOCK_STATE_META_MAP_PATH => '-1.21.111',
 		],
 		ProtocolInfo::PROTOCOL_1_21_124 => [
-			self::CANONICAL_BLOCK_STATES_PATH => '',
-			self::BLOCK_STATE_META_MAP_PATH => '',
+			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.111',
+			self::BLOCK_STATE_META_MAP_PATH => '-1.21.111',
 		],
 		ProtocolInfo::PROTOCOL_1_21_120 => [
-			self::CANONICAL_BLOCK_STATES_PATH => '',
-			self::BLOCK_STATE_META_MAP_PATH => '',
+			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.111',
+			self::BLOCK_STATE_META_MAP_PATH => '-1.21.111',
 		],
 		ProtocolInfo::PROTOCOL_1_21_111 => [
-			self::CANONICAL_BLOCK_STATES_PATH => '',
-			self::BLOCK_STATE_META_MAP_PATH => '',
+			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.111',
+			self::BLOCK_STATE_META_MAP_PATH => '-1.21.111',
 		],
 		ProtocolInfo::PROTOCOL_1_21_100 => [
 			self::CANONICAL_BLOCK_STATES_PATH => '-1.21.100',
